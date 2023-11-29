c81f8e6e9450386dcd9ad560691fbd991a913225
pzspsh <3277440871@qq.com>
2023-11-29 09:44:23 +0800

    update

--- Demo7/Demo/test.c
+++ Demo7/Demo/test.c
@@ -3,47 +3,34 @@
 @Author : pan
 @Time   : 2023-11-23 19:16:51
 */
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
-/*同构数*/
-void tg_num(int a, int b);
 int main()
 {
-    int a, b;
-    scanf("%d %d", &a, &b);
-    tg_num(a, b);
-}
-
-void tg_num(int a, int b)
-{
-    int ch, i, j_num, j, ch_num, flag, pd;
-    pd = 1;
-    for (i = a; i < b + 1; i++)
+    int m, n;
+    scanf("%d%d", &m, &n);
+    for (int i = 0; m <= n; m = m + 1)
     {
-        j = i;
-        ch = i * i;
-        flag = 1;
-        while (j != 0)
+        int b = m % 7;
+        if (b == 0)
         {
-            j_num = j % 10;
-            ch_num = ch % 10;
-            if (j_num != ch_num)
+            printf("%d is a multiple of 7\n", m);
+        }
+        int c = m;
+        while (c != 0)
+        {
+            int d = c % 10;
+            if (d == 7)
             {
-                flag = 0;
+                printf("%d contains 7\n", m);
                 break;
             }
-            j = j / 10;
-            ch = ch / 10;
-        }
-        if (flag == 1 && pd == 1)
-        {
-            printf("%d", i);
-            pd = 0;
-        }
-        else if (flag == 1)
-        {
-            printf(" %d", i);
+            else
+            {
+                c = c / 10;
+            }
         }
     }
+    return 0;
 }
