4aba7b6dc22ed3360e63371884f7eed92b96a063
pzspsh <3277440871@qq.com>
2024-02-28 15:40:51 +0800

    test

---
+++ Demo7/Demo/test5.c
@@ -0,0 +1,18 @@
+/*
+@File   : test5.c
+@Author : pan
+@Time   : 2024-02-28 15:38:30
+*/
+#include <stdio.h>
+int main(void)
+{
+    int num;
+    num = 1;
+    /* 一个简单的C程序 */
+    /* 定义一个名为num的变量 */
+    /* 为num赋一个值 */
+    printf("I am a simple ");
+    printf("computer.\n"); /* 使用printf()函数 */
+    printf("My favorite number is %d because it is first.\n", num);
+    return 0;
+}
